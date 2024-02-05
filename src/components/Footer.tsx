35b97de9625eb1229391fe560722c5d5b73bb6cb
irvanahmadp <irvan.ap1618@gmail.com>
2024-02-05 20:55:30 +0700

    fix minified react error #423

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Footer = () => {
+  const [year, setYear] = useState<number>();
+
+  useEffect(() => {
+    setYear(new Date().getFullYear())
+  }, [])
+
   return (
     <footer className="inset-0 top-auto flex h-16 items-center justify-center border-t border-slate-900 bg-slate-700 text-white dark:bg-slate-800">
-      Copyright &copy; {new Date().getFullYear()}
+      Copyright &copy; {year}
       &nbsp;
       <Link href="/">Irvan Ahmad P</Link>
     </footer>
