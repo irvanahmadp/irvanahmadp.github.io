a0f507591ab5d02603bd841baa89ae1908bf3ae5
irvanahmadp <irvan.ap1618@gmail.com>
2024-04-27 20:08:16 +0700

    feat: add google analytic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 function MyDocument() {
   return (
@@ -7,6 +8,23 @@ function MyDocument() {
       <body className="pt-14 antialiased accent-purple-400 dark:bg-slate-900 dark:text-slate-100 md:pt-20">
         <Main />
         <NextScript />
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-F47X5MD4V2`}
+        />
+        <Script
+          strategy="lazyOnload"
+          id="gtag-init"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-F47X5MD4V2');
+            `,
+          }}
+        />
       </body>
     </Html>
   );
