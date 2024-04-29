71feec8cb962a464cb901900320bff1059f6552e
irvanahmadp <irvan.ap1618@gmail.com>
2024-04-29 21:20:03 +0700

    fix: google analytic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { ThemeProvider } from "next-themes";
 import { LanguageProvider } from "src/hooks/useLanguage";
 import "src/styles/globals.css";
@@ -18,10 +19,26 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <ThemeProvider attribute="class" defaultTheme="system">
-      <LanguageProvider>
-        {getLayout(<Component {...pageProps} />)}
-      </LanguageProvider>
-    </ThemeProvider>
+      <ThemeProvider attribute="class" defaultTheme="system">
+        <LanguageProvider>
+          {getLayout(<Component {...pageProps} />)}
+        </LanguageProvider>
+
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-SK52D5FY7N`}
+        />
+        <Script
+          id="gtag-init"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-SK52D5FY7N');
+            `,
+          }}
+        />
+      </ThemeProvider>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 function MyDocument() {
   return (
@@ -8,23 +7,6 @@ function MyDocument() {
       <body className="pt-14 antialiased accent-purple-400 dark:bg-slate-900 dark:text-slate-100 md:pt-20">
         <Main />
         <NextScript />
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-SK52D5FY7N`}
-        />
-        <Script
-          strategy="lazyOnload"
-          id="gtag-init"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-SK52D5FY7N');
-            `,
-          }}
-        />
       </body>
     </Html>
   );
